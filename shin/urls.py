0c8d5be2c766474ad388f56e7532bb256d6a7822
Rifqi Rosyidi <rief.rosyidi@gmail.com>
2019-10-21 08:56:36 +0700

    static

--- shin/urls.py
+++ shin/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
 from . import views
 from django.contrib.staticfiles.urls import static
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,3 +10,6 @@ urlpatterns = [
     path('about/', views.about),
     path('', views.homepage)
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
