68eeed92a4fc746a7dc6643595f15d712f4ab590
Rifqi Rosyidi <rief.rosyidi@gmail.com>
2019-10-24 18:07:13 +0700

    article_detail

--- articles/urls.py
+++ articles/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 
+app_name = 'article'
 urlpatterns = [
     path('', views.article_list, name='list'),
     path('<slug:slug>/', views.article_detail, name='detail'),
